5ac276bda0baa874eff2b507899672e2a8ab8118
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-04-26 12:06:45 +0530

    v3 class not work in v4

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -158,11 +158,11 @@ function Login() {
             <h1 className="bg-gradient-to-b from-white to-[#c084fc] bg-clip-text text-transparent font-medium text-start md:text-center mx-0 md:mx-auto font-rmneue w-3/4 text-7xl max-md:text-3xl max-md:text-[calc(40px+0.5vw)]">
               Visualize Your Progress
             </h1>
-            <p className="!text-[#f8f8f8]/80 font-light font-rmneue text-start md:text-center mt-2">Organize tasks, track progress, and stay focused with a visual workflow.</p>
+            <p className="!text-[#f8f8f8] opacity-80 font-light font-rmneue text-start md:text-center mt-2">Organize tasks, track progress, and stay focused with a visual workflow.</p>
             <div className="buttons flex justify-center">
               <button
                 onClick={openLoginModal}
-                className="rounded-3xl backdrop-blur-[1.5px] my-5 me-2 border border-[#f8f8f8]/20 text-white bg-white/5 py-2 sm:px-10 max-sm:px-4 font-normal"
+                className="rounded-3xl backdrop-blur-[1.5px] my-5 me-2 border border-[#f8f8f8] opacity-20 text-white bg-white/5 py-2 sm:px-10 max-sm:px-4 font-normal"
               >
                 Login
               </button>
