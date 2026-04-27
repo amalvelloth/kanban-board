a60fd6edcc061fc0b0410d7a4da1041c38865f6e
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-04-27 17:51:54 +0530

    navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar({ className }) {
 
 
   return (
-    <nav className={`mt-0 border border-white/20 shadow-black/20 rounded-xl md:mt-4 md:backdrop-blur-2xl rounded h-16 w-full md:w-3xl md:mx-auto md:left-0 md:right-0 absolute px-4 font-rmneue flex items-center justify-between z-10 ${className}`}>
+    <nav className={`glass-effect-1 mt-0 rounded-xl md:mt-4 h-16 w-full md:w-3xl md:mx-auto md:left-0 md:right-0 !absolute px-4 font-rmneue flex items-center justify-between z-10 ${className}`}>
       {/* {!isHomePage && <BurgerMenu />} */}
       <a href="/" class="flex items-center gap-3">
         <img src={Logo} className='h-6 w-6 md:w-8 md:h-8 z-50' alt="" />
