20b1903624cbe1d4412c386dffbbd7aaebf46c63
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-05-03 19:05:58 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,8 +12,8 @@ function Navbar({ className }) {
 
 
   return (
-    <nav className={`mt-0 rounded-xl md:mt-4 h-16 w-full md:w-3xl md:mx-auto md:left-0 md:right-0 !absolute px-4 font-rmneue flex items-center justify-between z-10 ${className}`}>
-      {/* {!isHomePage && <BurgerMenu />} */}
+    <nav className={`login-navbar mt-0 rounded-xl md:mt-4 h-16 w-full md:w-3xl md:mx-auto md:left-0 md:right-0 !absolute px-4 font-rmneue flex items-center justify-between z-10 ${className}`}>
+      {/* {<BurgerMenu />} */}
       <a href="/" class="flex items-center gap-3">
         <img src={Logo} className='h-6 w-6 md:w-8 md:h-8 z-50' alt="" />
         <h1 className='text-xs font-carving text-[#b3bdf9] font-semibold md:text-lg z-50'>KANBAN</h1>
